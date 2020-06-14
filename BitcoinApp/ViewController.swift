3b6150fe676683d128361c711b469ca1c0fe58ad
erykszczesniak <eryks453@gmail.com>
2020-06-14 11:18:39 +0200

    Initial Commit

---
+++ BitcoinApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitcoinApp
+//
+//  Created by Eryk Szcześniak on 14/06/2020.
+//  Copyright © 2020 Eryk Szcześniak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
