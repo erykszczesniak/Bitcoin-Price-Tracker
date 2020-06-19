475f26c1c6317148f27313efa640aac515db92e9
erykszczesniak <eryks453@gmail.com>
2020-06-19 14:26:37 +0200

    Bitcoin App

--- BitcoinViewController.swift
+++ BitcoinViewController.swift
@@ -11,35 +11,66 @@ import UIKit
 class BitcoinViewController: UIViewController {
     
     
+    @IBOutlet weak var usdLabel: UILabel!
+    @IBOutlet weak var yenLabel: UILabel!
+    @IBOutlet weak var euroLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        getPrice()
         
+    }
+        
+      
+    func getPrice() {
         if let url = URL(string:  "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR"){
-            URLSession.shared.dataTask(with: url){(data:Data?,response:URLResponse?,error:Error?) in
-                
-                if error == nil{
-                    if data != nil {
-                 if let json = try? JSONSerialization.jsonObject(with: data!, options: []) as?
-                    [String:Double] {
-                    if let usdPrice = json["USD"]{
-                        print(usdPrice)
-                    }
-                    if let eurPrice = json["EUR"]{
-                        print(eurPrice)
-                    }
-                    if let jpyPrice = json["JPY"]{
-                        print(jpyPrice)
-                    }
-                        }
-                    }
-                    
-                } else {
-                    print("We have an error")
-                }
-                
-            }.resume()
+                  URLSession.shared.dataTask(with: url){(data:Data?,response:URLResponse?,error:Error?) in
+                      
+                      if error == nil{
+                          
+                          if data != nil {
+                       if let json = try? JSONSerialization.jsonObject(with: data!, options: []) as?
+                          [String:Double] {
+                          DispatchQueue.main.async {
+                          
+                          if let usdPrice = json["USD"]{
+                           
+                            self.usdLabel.text = self.getStringFor(price: usdPrice, currencyCode:"USD")
+                            
+                           }
+                          if let eurPrice = json["EUR"]{
+                              self.euroLabel.text = "€:\(eurPrice)"
+                          }
+                          if let jpyPrice = json["JPY"]{
+                              self.yenLabel.text = "¥:\(jpyPrice)"
+                          }
+                              }
+                          }
+                          
+                      }
+                          
+                      }   else {
+                          print("We have an error")
+                      }
+                  }.resume()
+              }
+              
+              }
+    func getStringFor(price:Double, currencyCode:String) -> String {
+        let formatter = NumberFormatter()
+                                 formatter.numberStyle = .currency
+                                 formatter.currencyCode = currencyCode
+        if let priceString = formatter.string(from: NSNumber(value: price)){
+            return priceString
         }
-        // Do any additional setup after loading the view.
-       
-}
+        return "Error"
+        
+    }
+        
+    
+    @IBAction func refresh(_ sender: Any) {
+        getPrice()
+    }
 }
